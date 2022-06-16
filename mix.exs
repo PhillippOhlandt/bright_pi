af74c0f8e2b75228b5c05a90b4a4f0e5416885ca
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2022-06-16 21:56:25 +0200

    release version 1.0.0

--- mix.exs
+++ mix.exs
@@ -11,7 +11,9 @@ defmodule BrightPi.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
       docs: docs(),
+      package: package(),
       source_url: "https://github.com/PhillippOhlandt/bright_pi",
       homepage_url: "https://github.com/PhillippOhlandt/bright_pi"
     ]
@@ -24,6 +26,13 @@ defmodule BrightPi.MixProject do
     ]
   end
 
+  defp description do
+    """
+    BrightPi is a library to talk to the "Pi Supply Bright Pi" board
+    featuring 4 white LEDs and 4 pairs of two infrared LEDs.
+    """
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -31,6 +40,14 @@ defmodule BrightPi.MixProject do
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:telemetry, "~> 1.0"}
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Phillipp Ohlandt"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/PhillippOhlandt/bright_pi"}
+    }
   end
 
   defp docs do
